01ecc3afb04942ccf4f628161fd2f610dd47ddd8
Marvin Block <mabl@dcc.ruhr>
2025-02-13 11:06:16 +0100

    added automated slotty salute

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,43 @@
-import { Client } from "discord.js";
-import { config } from "./config";
-import { commands } from "./commands";
-import { deployCommands } from "./deploy-commands";
+import { Client } from 'discord.js';
+import { commands } from './commands';
+import { config } from './config';
+import { deployCommands } from './deploy-commands';
 
 const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+  intents: ['Guilds', 'GuildMessages', 'DirectMessages', 'MessageContent'],
 });
 
-client.once("ready", () => {
-  console.log("Discord bot is ready! 🤖");
+client.once('ready', () => {
+  console.log('Discord bot is ready! 🤖');
 });
 
-client.on("guildCreate", async (guild) => {
+client.on('guildCreate', async (guild) => {
   await deployCommands({ guildId: guild.id });
 });
 
-client.on("guildAvailable", async (guild) => {
+client.on('guildAvailable', async (guild) => {
   await deployCommands({ guildId: guild.id });
 });
 
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-        return;
-    }
-    const { commandName } = interaction;
-    if (commands[commandName as keyof typeof commands]) {
-        commands[commandName as keyof typeof commands].execute(interaction);
-    }
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(interaction);
+  }
+});
+
+client.on('messageCreate', async (message) => {
+  if (
+    message.content.startsWith('<:salute:1335591427031306342>') &&
+    !message.author.bot
+  ) {
+    message.channel.send(
+      '<:salute:1335591427031306342> <:slotty:1336010394829066240>'
+    );
+  }
 });
 
 client.login(config.DISCORD_TOKEN);
-
