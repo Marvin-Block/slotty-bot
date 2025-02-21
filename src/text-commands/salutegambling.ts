81890b3393557c9b61e89178d0b80d072abbd6a7
Marvin Block <marvin.block1998@gmail.com>
2025-02-21 20:03:36 +0100

    fixed reply ping

--- src/text-commands/salutegambling.ts
+++ src/text-commands/salutegambling.ts
@@ -57,31 +57,44 @@ export async function run(
     addToDB(message.author, 4, rng.auditRecord);
     const attachment = new AttachmentBuilder("./assets/supreme_salute.gif");
     setTimeout(() => {
-      msg.edit({ content: "", files: [attachment] });
+      msg.edit({
+        content: "",
+        files: [attachment],
+        allowedMentions: { repliedUser: false },
+      });
     }, settings.delay);
   } else if (rng.number >= 495 && rng.number <= 505) {
     // Rarity 3 - Legendary - 1%
     addToDB(message.author, 3, rng.auditRecord);
 
     setTimeout(() => {
-      msg.edit(slottyGif);
+      msg.edit({ content: slottyGif, allowedMentions: { repliedUser: false } });
       setTimeout(() => {
-        msg.edit(slottyGif + slottyGif);
+        msg.edit({
+          content: slottyGif + slottyGif,
+          allowedMentions: { repliedUser: false },
+        });
         setTimeout(() => {
-          msg.edit(slottyGif + slottyGif + slottyGif);
+          msg.edit({
+            content: slottyGif + slottyGif + slottyGif,
+            allowedMentions: { repliedUser: false },
+          });
           setTimeout(() => {
-            msg.edit(
-              slottyGif +
+            msg.edit({
+              content:
+                slottyGif +
                 slottyGif +
                 slottyGif +
                 "\n" +
                 slottyGif +
                 salute +
-                slottyGif
-            );
+                slottyGif,
+              allowedMentions: { repliedUser: false },
+            });
             setTimeout(() => {
-              msg.edit(
-                slottyGif +
+              msg.edit({
+                content:
+                  slottyGif +
                   slottyGif +
                   slottyGif +
                   "\n" +
@@ -91,8 +104,9 @@ export async function run(
                   "\n" +
                   slottyGif +
                   slottyGif +
-                  slottyGif
-              );
+                  slottyGif,
+                allowedMentions: { repliedUser: false },
+              });
             }, settings.delay);
           }, settings.delay);
         }, settings.delay);
@@ -105,13 +119,22 @@ export async function run(
     // Rarity 2 - Epic - 5%
     addToDB(message.author, 2, rng.auditRecord);
     setTimeout(() => {
-      msg.edit(salute);
+      msg.edit({ content: salute, allowedMentions: { repliedUser: false } });
       setTimeout(() => {
-        msg.edit(salute + slottyGif);
+        msg.edit({
+          content: salute + slottyGif,
+          allowedMentions: { repliedUser: false },
+        });
         setTimeout(() => {
-          msg.edit(salute + slottyGif + slottyGif);
+          msg.edit({
+            content: salute + slottyGif + slottyGif,
+            allowedMentions: { repliedUser: false },
+          });
           setTimeout(() => {
-            msg.edit(salute + slottyGif + slottyGif + slottyGif);
+            msg.edit({
+              content: salute + slottyGif + slottyGif + slottyGif,
+              allowedMentions: { repliedUser: false },
+            });
           }, settings.delay);
         }, settings.delay);
       }, settings.delay);
@@ -123,18 +146,24 @@ export async function run(
     // Rarity 1 - Rare - 15%
     addToDB(message.author, 1, rng.auditRecord);
     setTimeout(() => {
-      msg.edit(salute);
+      msg.edit({ content: salute, allowedMentions: { repliedUser: false } });
       setTimeout(() => {
-        msg.edit(salute + slottyGif);
+        msg.edit({
+          content: salute + slottyGif,
+          allowedMentions: { repliedUser: false },
+        });
       }, settings.delay);
     }, settings.delay);
   } else {
     // Rarity 0 - Normal - 78.8%
     addToDB(message.author, 0, rng.auditRecord);
     setTimeout(() => {
-      msg.edit(salute);
+      msg.edit({ content: salute, allowedMentions: { repliedUser: false } });
       setTimeout(() => {
-        msg.edit(salute + slotty);
+        msg.edit({
+          content: salute + slotty,
+          allowedMentions: { repliedUser: false },
+        });
       }, settings.delay);
     }, settings.delay);
   }
